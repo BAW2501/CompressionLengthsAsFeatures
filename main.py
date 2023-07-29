49fd932dc3bc20312a40b60ef3bdce47b4ea9108
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2023-07-29 09:41:21 +0100

    fastest implementation

--- main.py
+++ main.py
@@ -2,37 +2,37 @@ import gzip
 import pickle
 import time
 import numpy as np
-
-
+from joblib import Parallel, delayed
+from sklearn.neighbors import KNeighborsClassifier
 
 with open(f"sentiment-dataset-500.pickle", "rb") as f:
-    dataset = pickle.load(f)
-
-train_x, train_y, test_x, test_y = dataset
-
+    train_x, train_y, test_x, test_y  = pickle.load(f)
+    
+train_x = [s.encode() for s in train_x]
+test_x = [s.encode() for s in test_x]
 
 def compressed_len(s):
-    return len(gzip.compress(s.encode()))
+    return len(gzip.compress(s))
 
+def ncd_cdist_parallel(x1, x2):
+    n, m = len(x1), len(x2)
+    x = x1 + x2
+    len_x = Parallel(n_jobs=-1)(delayed(compressed_len)(s) for s in x) # combined for combined threadpool
+    len_x1, len_x2 = len_x[:n], len_x[n:] # for readability can be avoided using n+j 
+    def compute_dist(i, j):
+        return (compressed_len(x1[i] + b' ' + x2[j]) - min(len_x1[i], len_x2[j])) / max(len_x1[i], len_x2[j])
+    
+    dist_mat = Parallel(n_jobs=-1)(delayed(compute_dist)(i, j) for i in range(n) for j in range(m))
 
-def fast_ncd_cdist(x1, x2):
-    n = len(x1)
-    m = len(x2)
-    len_x1sample = [compressed_len(sample) for sample in x1]
-    len_x2sample = [compressed_len(sample) for sample in x2]
-    dist_mat = np.zeros((n, m))
+    return np.reshape(dist_mat, (n, m))
 
-    for i in range(n):
-        for j in range(m):
-            compressed_len_ij = compressed_len(f'{x1[i]} {x2[j]}')
-            min_len = min(len_x1sample[i], len_x2sample[j])
-            max_len = max(len_x1sample[i], len_x2sample[j])
-            dist_mat[i][j] = (compressed_len_ij - min_len) / max_len
-    return dist_mat
-
-start = time.time()
-train_ncd = fast_ncd_cdist(train_x, train_x)
-test_ncd = fast_ncd_cdist(test_x, train_x)
-print(f"Time taken: {time.time() - start}")
-
-
+# for _ in range(10):
+#     start = time.time()
+#     train_ncd = ncd_cdist_parallel(train_x, train_x)
+#     test_ncd = ncd_cdist_parallel(test_x, train_x)
+#     print("Time:", time.time() - start)
+train_ncd = ncd_cdist_parallel(train_x, train_x)
+test_ncd = ncd_cdist_parallel(test_x, train_x)
+neigh = KNeighborsClassifier(n_neighbors=7)
+neigh.fit(train_ncd, train_y)
+print("Accuracy:", neigh.score(test_ncd, test_y))
